16904b684f060e551108070e0cbe02ada4b7f6ee
NataliR55 <rsn7428@yandex.ru>
2023-06-04 21:00:16 +0600

    add add-booking

--- src/main/java/ru/practicum/shareit/booking/model/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -4,11 +4,15 @@ import lombok.Data;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
-
+@Entity
 @Data
 public class Booking {
-    private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
     private LocalDateTime start;
     private LocalDateTime end;
     private Item item;
--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -5,21 +5,30 @@ import lombok.Builder;
 import lombok.Data;
 import ru.practicum.shareit.user.model.User;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+@Entity
+@Table(name = "items")
 @Data
 @Builder
 @AllArgsConstructor
 public class Item {
-    private long id;
-    @NotBlank
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", nullable = false)
     private String name;
-    @NotBlank
+    @Column(name = "description", nullable = false)
     private String description;
-    @NotNull
+    @Column(name = "is_available", nullable = false)
     private Boolean available;
-    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private User owner;
-    private Long request;
+    @ManyToOne
+    @JoinColumn(name = "request_id")
+    private ItemRequest request;
+
 }

04311abed1ddd453bb9f0436d94e58a963eb2c28
NataliR55 <rsn7428@yandex.ru>
2023-06-06 07:55:27 +0600

    tests Postman passed

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -3,18 +3,33 @@ package ru.practicum.shareit.user.model;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 @Data
 @Builder
+@Entity
+@Table(name = "users", schema = "public")
 public class User {
-    private long id;
+    @Id
+    private Long id;
     @NotBlank
     @Pattern(regexp = "\\S+")
+    @Column(name = "name", nullable = false)
     private String name;
     @Email(message = "Field: Email must have the format EMAIL!")
     @NotBlank(message = "Field: Email must be filled!")
+    @Column(name = "email", length = 128, nullable = false, unique = true)
     private String email;
+
+    //@Enumerated(EnumType.STRING)
+    //private UserState state;
+    //@Column(name = "registration_date")
+    //private Instant registrationDate = Instant.now();
+
 }
